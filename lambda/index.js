a376b13822a77a9505e0271c9186fed44a9e2769
yashpuneet <yash.puneet02@gmail.com>
2022-07-26 00:28:30 +0800

    Added Lambda function file with basic required intents and initial launch greeting intent processing

---
+++ lambda/index.js
@@ -0,0 +1,105 @@
+const Alexa = require('ask-sdk-core');
+const i18n = require('i18next');
+const languageStrings = require('./languageStrings');
+const sprintf = require('i18next-sprintf-postprocessor');
+
+const HelloHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return (request.type === 'LaunchRequest') 
+            || (request.type === 'IntentRequest'
+                && request.intent.name === 'HelloIntent');
+    },
+    handle(handlerInput) {
+        const speechOutput = "Hello, welcome to task motivator!";
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .getResponse();
+    },
+};
+
+const HelpHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('HELP_MESSAGE'))
+      .reprompt(requestAttributes.t('HELP_REPROMPT'))
+      .getResponse();
+  },
+};
+
+const FallbackHandler = {
+  // The FallbackIntent can only be sent in those locales which support it,
+  // so this handler will always be skipped in locales where it is not supported.
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.FallbackIntent';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('FALLBACK_MESSAGE'))
+      .reprompt(requestAttributes.t('FALLBACK_REPROMPT'))
+      .getResponse();
+  },
+};
+
+const ExitHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('STOP_MESSAGE'))
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+    console.log(`Error stack: ${error.stack}`);
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('ERROR_MESSAGE'))
+      .reprompt(requestAttributes.t('ERROR_MESSAGE'))
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    HelloHandler,
+    HelpHandler,
+    ExitHandler,
+    FallbackHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
